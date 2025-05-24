af3b608c7e12ddf220a3545c74311dc0ef4740b2
siveozdemir6 <siveozdemir6@gmail.com>
2025-05-24 16:22:11 +0300

    Add project files.

---
+++ Classes/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public void WriteInfo()
+        {
+            Console.WriteLine("İsim: " + Name + " Soyisim: " + Surname + " Doğum Tarihi: " + BirthDate );
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using Classes;
+
+Person student1 = new Person();
+student1.Name = "Ted";
+student1.Surname = "Mosby";
+student1.BirthDate = new DateTime(1979, 7, 28);
+
+Person student2 = new Person();
+student2.Name = "Marshall";
+student2.Surname = "Eriksen";
+student2.BirthDate = new DateTime(1979, 3, 24);
+
+Person student3 = new Person();
+student3.Name = "Robin";
+student3.Surname = "Scherbatsky";
+student3.BirthDate = new DateTime(1981, 4, 26);
+
+Person teacher1 = new Person();
+teacher1.Name = "Lily";
+teacher1.Surname = "Aldrin";
+teacher1.BirthDate = new DateTime(1980, 9, 12);
+
+Person teacher2 = new Person();
+teacher2.Name = "Barney";
+teacher2.Surname = "Stinson";
+teacher2.BirthDate = new DateTime(1978, 3, 23);
+Console.WriteLine("----------------------ÖĞRENCİ BİLGİLERİ---------------------------");
+student1.WriteInfo();
+student2.WriteInfo();   
+student3.WriteInfo();
+Console.WriteLine("----------------------ÖĞRETMEN BİLGİLERİ--------------------------");
+teacher1.WriteInfo();
+teacher2.WriteInfo();
+
+
+
